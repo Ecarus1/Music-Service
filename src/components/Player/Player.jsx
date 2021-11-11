b339691fc896aad24b1fc49b2de643c0bf12444b
Ecarus1 <solodovnikov.andrey99@mail.ru>
2021-11-11 12:38:52 +0300

    Application bug
    Bug in the player component

--- src/components/Player/Player.jsx
+++ src/components/Player/Player.jsx
@@ -1,15 +1,66 @@
+import { useState, useEffect } from "react";
+
 import PlayerHeader from "../PlayerHeader/PlayerHeader";
 import PlayerInterface from "../PlayerInterface/PlayerInterface";
 import "./Player.scss";
 
 function Player() {
-    const original = "image/original.jpg";
+    // const original = "image/original.jpg";
+
+    const [songs, setSongs] = useState([{
+        title: "Самый дорогой",
+        artist: "Нервы",
+        imgSrc: "image/original.jpg",
+        src: "music/Nervi-SmiyDorogoiChelovek.mp3"
+    },
+    {
+        title: "Привет, лови",
+        artist: "Нервы",
+        imgSrc: "image/original2.jpg",
+        src: "music/Nervi-ProvetLovi.mp3"
+    },
+    {
+        title: "Sportlight",
+        artist: "Marshmello, LilPeep",
+        imgSrc: "image/original3.jpg",
+        src: "music/MarshmelloLilPeep-Spotlight.mp3"
+    }]);
+
+    const [currentSongIndex, setCurrentSongIndex] = useState(0);
+    // const [nextSongIndex, setNextSongIndex] = useState(currentSongIndex + 1);
+
+    // useEffect(() => {
+    //     setNextSongIndex(() => {
+    //         if (currentSongIndex + 1 > songs.length - 1) {
+    //             return;
+    //         } else {
+    //             return currentSongIndex + 1;
+    //         }
+    //     });
+    // }, [currentSongIndex]);
+
+    // return(
+    //     <div className="player">
+    //         <img className="player__image" src={songs[currentSongIndex].imgSrc} alt="" />
+    //         <PlayerHeader/>
+    //         <PlayerInterface 
+    //             // song={songs[currentSongIndex]}
+    //             // nextSong={songs[nextSongIndex]}
+    //             currentSongIndex={currentSongIndex}
+    //             setCurrentSongIndex={setCurrentSongIndex}
+    //             nextSongIndex={nextSongIndex}
+    //             songs={songs}/>
+    //     </div>
+    // );
 
     return(
         <div className="player">
-            <img className="player__image" src={original} alt="" />
+            <img className="player__image" src={songs[currentSongIndex].imgSrc} alt="" />
             <PlayerHeader/>
-            <PlayerInterface/>
+            <PlayerInterface
+                currentSongIndex={currentSongIndex}
+                setCurrentSongIndex={setCurrentSongIndex}
+                songs={songs}/>
         </div>
     );
 }
--- src/components/PlayerInterface/PlayerInterface.jsx
+++ src/components/PlayerInterface/PlayerInterface.jsx
@@ -1,15 +1,119 @@
+import React, {useState, useRef, useEffect} from "react";
+
 import "./PlayerInterface.scss";
 
-function PlayerInterface() {
+function PlayerInterface(props) {
+    const audioElem = useRef(null);
+
+    const [isPlaying, setIsPlaying] = useState(false);
+
+    // const audioId = document.getElementById("audio-id");
+    const audio = new Audio();
+    audio.src = props.songs[props.currentSongIndex].src;
+
+    useEffect(() => {
+        if (isPlaying) {
+            audioElem.current.play();
+            console.log("Играет");
+        } else {
+            audioElem.current.pause();
+            console.log("Остановлен");
+        }
+    });
+
+    const SkipSong = (forwards = true) => {
+        if (forwards) {
+            props.setCurrentSongIndex(() => {
+                let temp = props.currentSongIndex;
+                temp++;
+
+                if (temp > props.songs.length - 1) {
+                    temp = 0;
+                }
+
+                return temp;
+            });
+        } else {
+            props.setCurrentSongIndex(() => {
+                let temp = props.currentSongIndex;
+                temp--;
+
+                if (temp < 0) {
+                    temp = props.songs.length - 1;
+                }
+
+                return temp;
+            });
+        }
+    };
+
+    // const SkipSong = (forwards = true) => {
+    //     if (forwards) {
+    //         props.setCurrentSongIndex(() => {
+    //             let temp = props.currentSongIndex;
+    //             temp++;
+
+    //             if (temp > props.songs.length - 1) {
+    //                 temp = 0;
+    //             }
+
+    //             return temp;
+    //         });
+    //     } else {
+    //         props.setCurrentSongIndex(() => {
+    //             let temp = props.currentSongIndex;
+    //             temp--;
+
+    //             if (temp < 0) {
+    //                 temp = props.songs.length - 1;
+    //             }
+                
+    //             return temp;
+    //         });
+    //     }
+    // };
+
+    // return(
+    //     <div className="player__interface">
+    //         <audio src={props.songs[props.currentSongIndex].src} ref={audioElem}></audio>
+
+    //         <div className="interface__title">
+    //             <h3>{props.songs[props.currentSongIndex].title}</h3>
+    //         </div>
+
+    //         <div className="interface__settings">
+    //             <i className="material-icons interface__setting">replay</i>
+    //             <p className="interface__setting">{props.songs[props.currentSongIndex].artist}</p>
+    //             <i className="material-icons interface__setting">volume_up</i>
+    //         </div>
+
+    //         <div className="interface__progress">
+    //             <div className="interface__played">
+    //                 <div className="interface__circle"></div>
+    //             </div>
+    //         </div>
+
+    //         <div className="interface__controls">
+    //             <i className="material-icons icon" onClick={() => SkipSong()}>skip_previous</i>
+    //             <i className="material-icons icon" onClick={() => setIsPlaying(!isPlaying)}>play_arrow</i>
+    //             <i className="material-icons icon" onClick={() => SkipSong(false)}>skip_next</i>
+    //         </div>
+    //     </div>
+    // );   
+    // const {songs, currentSongIndex} = props;
     return(
         <div className="player__interface">
+            {/* <audio src={props.songs[props.currentSongIndex].src} ref={audioElem}>
+            </audio> */}
+            <audio id="audio-id" ref={audioElem}></audio>
+
             <div className="interface__title">
-                <h3>Самый дорогой</h3>
+                <h3>{props.songs[props.currentSongIndex].title}</h3>
             </div>
 
             <div className="interface__settings">
                 <i className="material-icons interface__setting">replay</i>
-                <p className="interface__setting">Нервы</p>
+                <p className="interface__setting">{props.songs[props.currentSongIndex].artist}</p>
                 <i className="material-icons interface__setting">volume_up</i>
             </div>
 
@@ -20,9 +124,9 @@ function PlayerInterface() {
             </div>
 
             <div className="interface__controls">
-                <i className="material-icons icon">skip_previous</i>
-                <i className="material-icons icon">play_arrow</i>
-                <i className="material-icons icon">skip_next</i>
+                <i className="material-icons icon" onClick={() => SkipSong(false)}>skip_previous</i>
+                <i className="material-icons icon" onClick={() => setIsPlaying(!isPlaying)}>{isPlaying ? "pause" : "play_arrow"}</i>
+                <i className="material-icons icon" onClick={() => SkipSong()}>skip_next</i>
             </div>
         </div>
     );   
